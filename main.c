a8a3754c76398b74cd78d0caf0b72e151c212dfc
Momin Hosan Emon <mominhasanemon@gmail.com>
2024-07-17 14:18:59 +0600

    second commit

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 //Program to check if your number is prime or not.
-int main(){
+void main(){
     int active= 1;
-    while(active!=0){
-    printf("Enter a number to check if it is prime or not: ");
+    while(active){
+    printf("\nEnter a number to check if it is prime or not: ");
     int num;
     scanf("%d", &num);
     int result = 0;
@@ -14,18 +14,17 @@ int main(){
     }
     switch (result){
     case 2:{
-        printf("%d is a prime number.", num);
-        printf("\nTo turn off this program enter 0: ");
+        printf("\n%d is a prime number.", num);
+        printf("\n\nTo turn off this program enter 0, otherwise if you want to continue enter 1: ");
         scanf("%d", &active);
         break;
     }
     default:{
-        printf("%d is not a prime number", num);
-        printf("\nTo turn off this program enter 0: ");
+        printf("\n%d is not a prime number", num);
+        printf("\n\nTo turn off this program enter 0, otherwise if you want to continue enter 1: ");
         scanf("%d", &active);
         break;
     }
     }
     }
-    return 0;
 }
